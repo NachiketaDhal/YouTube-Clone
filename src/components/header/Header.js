6f5469b266d0cbfefdc0ad0e24c1967a72b410e5
Nachiketa Dhal <bnachiketa26@gmail.com>
2021-06-27 11:33:11 +0530

    Update header component

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -1,9 +1,35 @@
 import React from 'react';
+import { FaBars } from 'react-icons/fa';
+import { AiOutlineSearch } from 'react-icons/ai';
+import { MdNotifications, MdApps } from 'react-icons/md';
 
 import './_header.scss';
 
 const Header = () => {
-  return <div className="border border-dark">Header</div>;
+  return (
+    <div className="border border-dark header">
+      <FaBars className="header__menu" size={26} />
+      <img
+        src="http://pngimg.com/uploads/youtube/youtube_PNG2.png"
+        alt="youtube"
+        className="header__logo"
+      />
+      <form>
+        <input type="text" placeholder="Search" />
+        <button type="submit">
+          <AiOutlineSearch size={22} />
+        </button>
+      </form>
+      <div className="header__icons">
+        <MdNotifications size={28} />
+        <MdApps size={28} />
+        <img
+          src="https://www.pngkey.com/png/full/114-1149878_setting-user-avatar-in-specific-size-without-breaking.png"
+          alt="avatar"
+        />
+      </div>
+    </div>
+  );
 };
 
 export default Header;
