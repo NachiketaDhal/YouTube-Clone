57913c07844a061be5e92e64ec49447202ebbf24
Nachiketa Dhal <bnachiketa26@gmail.com>
2021-07-01 10:47:23 +0530

    Remove infinite scroll

--- src/screen/homeScreen/HomeScreen.js
+++ src/screen/homeScreen/HomeScreen.js
@@ -5,10 +5,10 @@ import CategoriesBar from '../../components/categoriesBar/CategoriesBar';
 import { useDispatch, useSelector } from 'react-redux';
 import {
   getMostPopularVideos,
-  getVideosByCategory,
+  // getVideosByCategory,
 } from '../../redux/actions/videos.action';
 
-import InfiniteScroll from 'react-infinite-scroll-component';
+// import InfiniteScroll from 'react-infinite-scroll-component';
 import SkeletonVideo from '../../components/skeleton/SkeletonVideo';
 
 const HomeScreen = () => {
@@ -21,18 +21,18 @@ const HomeScreen = () => {
     state => state.homeVideos
   );
 
-  const fetchData = () => {
-    if (activeCategory === 'All') dispatch(getMostPopularVideos());
-    else {
-      dispatch(getVideosByCategory(activeCategory));
-    }
-  };
+  // const fetchData = () => {
+  //   if (activeCategory === 'All') dispatch(getMostPopularVideos());
+  //   else {
+  //     dispatch(getVideosByCategory(activeCategory));
+  //   }
+  // };
 
   return (
     <Container>
       <CategoriesBar />
 
-      <InfiniteScroll
+      {/* <InfiniteScroll
         dataLength={videos.length}
         next={fetchData}
         hasMore={true}
@@ -40,19 +40,19 @@ const HomeScreen = () => {
           <div className="spinner-border text-danger d-block mx-auto"></div>
         }
         className="row"
-      >
-        {!loading
-          ? videos.map(video => (
-              <Col lg={3} md={4}>
-                <Video video={video} key={video.id} />
-              </Col>
-            ))
-          : [...Array(20)].map(() => (
-              <Col lg={3} md={4}>
-                <SkeletonVideo />
-              </Col>
-            ))}
-      </InfiniteScroll>
+      > */}
+      {!loading
+        ? videos.map(video => (
+            <Col lg={3} md={4}>
+              <Video video={video} key={video.id} />
+            </Col>
+          ))
+        : [...Array(20)].map(() => (
+            <Col lg={3} md={4}>
+              <SkeletonVideo />
+            </Col>
+          ))}
+      {/* </InfiniteScroll> */}
     </Container>
   );
 };
